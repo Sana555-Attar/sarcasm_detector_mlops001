b8d641488f634193e4dbe6122f52284d260102b6
Sana555-Attar <sanaattar1857@gmail.com>
2023-06-05 20:46:32 +0530

    added correct image name

--- sarcasm_app1.py
+++ sarcasm_app1.py
@@ -145,7 +145,7 @@ def push_file_to_github():
     folder_name = "updated_new_version_data"
 
     # GitHub access token (you can generate one in your GitHub account settings)
-    access_token ='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'
+    access_token ='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'
 
     # Create a PyGithub instance using the access token
     g = Github(access_token)
